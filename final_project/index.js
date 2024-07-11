88f8bd92db39e86f5957f0f232a44cc9aca9ea26
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2024-07-11 20:55:22 +0530

    new

--- final_project/index.js
+++ final_project/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
@@ -12,35 +11,26 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-     // Check if session exists
-  if (!req.session || !req.session.accessToken) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  try {
-    // Verify the access token stored in the session
-    const decoded = jwt.verify(req.session.accessToken, "your_secret_key");
-    req.user = decoded;
-    next();
-  } catch (error) {
-    // If the token is invalid, clear the session and send an error response
-    req.session.destroy();
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+    if(req.session.authorization) { //get the authorization object stored in the session
+        token = req.session.authorization['accessToken']; //retrieve the token from authorization object
+        jwt.verify(token, "access",(err,user)=>{ //Use JWT to verify token
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }
 });
 
-const arr = [];
-const user = "john";
-const pass = "pass";
-const user1 = "john1";
-const pass1 = "pass1";
-arr.push({user, pass});
-arr.push({user1, pass1});
 
-const PORT =5005;
+const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>{console.log("Server is running on port " + PORT)});
